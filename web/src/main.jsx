c7fbaf505efc5a3c752624a17eda6ed92e797677
vijayakanthang <vijaykanthan03@gmail.com>
2025-07-29 09:52:14 +0530

    updated env

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -4,7 +4,9 @@ import './index.css'
 import App from './App.jsx'
 import {GoogleOAuthProvider} from '@react-oauth/google'
 
-const CLIENT_ID = "1033447667626-80ks5tf3polk9q1j1kg1p4hs127smoll.apps.googleusercontent.com"
+
+
+const CLIENT_ID = import.meta.env.C_ID;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
